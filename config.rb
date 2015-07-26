e121a6358ac4463ff012a8b1bd3b7dad0785e30e
Matteo Bigoi <bigo@crisidev.org>
2015-07-27 00:21:26 +0100

    Shared registry

--- config.rb
+++ config.rb
@@ -76,7 +76,7 @@ $vm_cpus = 2
 # $shared_folders = {'/path/on/host' => '/path/on/guest', '/home/foo/app' => '/app'}
 # or, to map host folders to guest folders of the same name,
 # $shared_folders = Hash[*['/home/foo/app1', '/home/foo/app2'].map{|d| [d, d]}.flatten]
-#$shared_folders = {'/Users/bigo/.share' => '/share'}
+$shared_folders = {'/Users/bigo/.share' => '/share'}
 
 # Enable port forwarding from guest(s) to host machine, syntax is: { 80 => 8080 }, auto correction is enabled by default.
 #$forwarded_ports = {}

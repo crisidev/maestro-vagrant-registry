e4084211ae15c5c3d60fb9e501d547413beda22e
Matteo Bigoi <bigo@crisidev.org>
2015-07-27 17:13:06 +0100

    Registry does not need an etcd cluster

--- config.rb
+++ config.rb
@@ -8,23 +8,23 @@ $new_discovery_url="https://discovery.etcd.io/new?size=#{$num_instances}"
 # To automatically replace the discovery token on 'vagrant up', uncomment
 # the lines below:
 #
-if File.exists?('user-data') && ARGV[0].eql?('up')
-  require 'open-uri'
-  require 'yaml'
+#if File.exists?('user-data') && ARGV[0].eql?('up')
+  #require 'open-uri'
+  #require 'yaml'
 
-  token = open($new_discovery_url).read
+  #token = open($new_discovery_url).read
 
-  data = YAML.load(IO.readlines('user-data')[1..-1].join)
-  if data['coreos'].key? 'etcd'
-    data['coreos']['etcd']['discovery'] = token
-  end
-  if data['coreos'].key? 'etcd2'
-    data['coreos']['etcd2']['discovery'] = token
-  end
+  #data = YAML.load(IO.readlines('user-data')[1..-1].join)
+  #if data['coreos'].key? 'etcd'
+    #data['coreos']['etcd']['discovery'] = token
+  #end
+  #if data['coreos'].key? 'etcd2'
+    #data['coreos']['etcd2']['discovery'] = token
+  #end
 
-  yaml = YAML.dump(data)
-  File.open('user-data', 'w') { |file| file.write("#cloud-config\n\n#{yaml}") }
-end
+  #yaml = YAML.dump(data)
+  #File.open('user-data', 'w') { |file| file.write("#cloud-config\n\n#{yaml}") }
+#end
 
 #
 # coreos-vagrant is configured through a series of configuration
